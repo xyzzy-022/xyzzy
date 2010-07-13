e5831c772fa4293caeb88e2940ac23ddeca77dfc
NANRI <southly@gmail.com>
2010-07-14 03:29:14 +0900

    xyzzy.src : fix format "~T".

--- src/lprint.cc
+++ src/lprint.cc
@@ -2844,7 +2844,7 @@ Format::tabulate (wStream &stream)
         {
           if (!colinc)
             return;
-          colnum += (col - colnum + colinc - 1) / colinc * colinc;
+          colnum += (col - colnum + colinc) / colinc * colinc;
         }
       stream.fill (' ', colnum - col);
     }
