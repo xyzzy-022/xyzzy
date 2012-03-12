8436f7ea27acde9c40d75f792c25d82c03a24914
fred waldo <x022235@gmail.com>
2012-03-13 00:16:39 +0900

    ログレベルのデフォルトを normal に変更. #15

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 
 @if "%1"=="" (set CONFIGURATION=Release) else (set CONFIGURATION=%1)
 @if "%2"=="" (set TARGET=Build) else (set TARGET=%2)
-@if "%3"=="" (set VERBOSITY=quiet) else (set VERBOSITY=%3)
+@if "%3"=="" (set VERBOSITY=normal) else (set VERBOSITY=%3)
 
 msbuild xyzzy.sln /nologo /p:Configuration=%CONFIGURATION% /target:%TARGET% /verbosity:%VERBOSITY%
