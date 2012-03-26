2f58603a9e7a47f333f36a5d273c2b8d4318072d
fred waldo <x022235@gmail.com>
2012-03-26 22:21:26 +0900

    LEGAL ファイルのファイル名変更に追従. #160

--- dist.bat
+++ dist.bat
@@ -36,7 +36,7 @@ call bytecompile.bat || exit /b 1
 
 xcopy /F /G /H /R /K /Y *.exe %DISTDIR%
 xcopy /F /G /H /R /K /Y LICENSE %DISTDIR%\docs\
-xcopy /F /G /H /R /K /Y LEGAL %DISTDIR%\docs\
+xcopy /F /G /H /R /K /Y LEGAL.md %DISTDIR%\docs\
 xcopy /F /G /H /R /K /Y /S /E lisp %DISTDIR%\lisp\
 xcopy /F /G /H /R /K /Y /S /E etc %DISTDIR%\etc\
 xcopy /F /G /H /R /K /Y /S /E docs %DISTDIR%\docs\
