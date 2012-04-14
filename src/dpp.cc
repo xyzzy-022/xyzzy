e82d1f885ea33d737bf271ce92a66848e08233c5
x022235 <x022235@gmail.com>
2012-04-15 02:27:21 +0900

    無駄な stdafx.h の include を削除. #215

--- src/dpp.cc
+++ src/dpp.cc
@@ -1,4 +1,9 @@
-#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <io.h>
 
 static FILE *fi, *fo;
 #define MAXARGS 2
