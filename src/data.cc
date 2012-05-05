83a990da74760c6071740fcc77b172e3d126d694
mumurik <mumurik2@hotmail.co.jp>
2012-05-05 22:22:16 +0900

    ダンプ時にpackageのdocumentationフィールドが無くなってしまう問題の修正。 fix #266

--- src/data.cc
+++ src/data.cc
@@ -1986,6 +1986,7 @@ dump_object (FILE *fp, const lpackage *d, int n,
         writef (fp, d->shadowings);
         writef (fp, d->internal);
         writef (fp, d->external);
+        writef (fp, d->documentation);
       }
 }
 
@@ -2003,6 +2004,7 @@ rdump_object (FILE *fp, lpackage *d, int n,
         d->shadowings = readl (fp);
         d->internal = readl (fp);
         d->external = readl (fp);
+        d->documentation = readl (fp);
       }
 }
 
