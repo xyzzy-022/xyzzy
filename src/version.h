2a51a532dc2cc5b0a08ce309687d50b5df35132b
x022235 <x022235@gmail.com>
2013-04-04 18:27:55 +0900

    Copyright の年を更新 (#384)

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 # define PROGRAM_APP_USER_MODEL_ID PROGRAM_NAME
 // ファイルバージョンには複数書けない
 // # define PROGRAM_COPYRIGHT "Copyright (C) 1996-2005 T.Kamei"
-# define PROGRAM_COPYRIGHT "Copyright (C) 2012 xyzzy Project"
+# define PROGRAM_COPYRIGHT "Copyright (C) 2012-2013 xyzzy Project"
 
 # define PROGRAM_MAJOR_VERSION 0
 # define PROGRAM_MINOR_VERSION 2
